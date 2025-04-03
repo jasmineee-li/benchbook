54a5a97c131aedad326162e7d8bfc0538ffa5e3c
jasmineee-li <jasminexinzeli@gmail.com>
2025-04-03 15:02:18 -0400

    rebuild dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { ScrollableGuestbook } from "@/components/scrollable-guestbook"
+import { ScrollableGuestbook } from "@/components/scrollable-guestbook";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <ScrollableGuestbook />
     </main>
-  )
+  );
 }
-
